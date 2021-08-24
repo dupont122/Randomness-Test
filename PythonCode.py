4cc6bb2d4482c0f9ace09ede672e3e591702c714
Quenton Dupont <quentondupont@gmail.com>
2021-08-24 14:01:37 +0700

    Update PythonCode.py

--- PythonCode.py
+++ PythonCode.py
@@ -1,1 +1,13 @@
+import random
 
+lotteryNumbers = []
+
+for numRange in range (0,10):
+  numbers = random.randint(1,50)
+  while number in lotteryNumbers:
+    number = random.randint(1,50)
+  lotteryNumbers.append(number)
+
+lotteryNumbers.sort()
+
+print(lotteryNumbers)
